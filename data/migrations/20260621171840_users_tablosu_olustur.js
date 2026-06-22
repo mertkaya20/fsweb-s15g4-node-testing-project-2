e7b0ab917a82e8e7a1999ddcb358373337a07223
mertkaya20 <mertkaya.codes@gmail.com>
2026-06-22 15:27:01 +0300

    feat: add database config and migrations

---
+++ data/migrations/20260621171840_users_tablosu_olustur.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (tbl) => {
+    tbl.increments("id");
+    tbl.string("username", 50).notNullable().unique();
+    tbl.string("email", 100).notNullable().unique();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
+};
